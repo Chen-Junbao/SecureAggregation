e567b3a07a4135d5877040bc71640d0a7cd51008
ChenJunbao <chen.junbao@outlook.com>
2021-11-29 12:51:40 +0000

    feature: share keys

--- entities/server.py
+++ entities/server.py
@@ -1,5 +1,4 @@
 import pickle
-import struct
 import socket
 import logging
 import socketserver
@@ -9,32 +8,59 @@ from utils import SocketUtil
 
 class SignatureRequestHandler(socketserver.BaseRequestHandler):
     user_num = 0
-    signature_list = []
+    ka_pub_keys_map = {}    # {id: {c_pk: bytes, s_pk, bytes, signature: bytes}}
     U_1 = []
 
     def handle(self) -> None:
         # receive data from the client
         data = SocketUtil.recv_msg(self.request)
 
-        signature = pickle.loads(data)
+        msg = pickle.loads(data)
+        id = msg["id"]
+        del msg["id"]
 
-        SignatureRequestHandler.signature_list.append(signature)
-        SignatureRequestHandler.U_1.append(signature["id"])
+        SignatureRequestHandler.ka_pub_keys_map[id] = msg
+        SignatureRequestHandler.U_1.append(id)
 
-        received_num = len(SignatureRequestHandler.signature_list)
+        received_num = len(SignatureRequestHandler.U_1)
 
         logging.info("[%d/%d] | received %s's signature", received_num,
                      SignatureRequestHandler.user_num, self.client_address[0])
+
+
+class SecretShareRequestHandler(socketserver.BaseRequestHandler):
+    U_1_num = 0
+    ciphertexts_map = {}         # {id: ciphertexts}
+    U_2 = []
+
+    def handle(self) -> None:
+        # receive data from the client
+        data = SocketUtil.recv_msg(self.request)
+
+        msg = pickle.loads(data)
+        id = msg[0]
+
+        SecretShareRequestHandler.ciphertexts_map[id] = msg[1]
+        SecretShareRequestHandler.U_2.append(id)
+
+        received_num = len(SecretShareRequestHandler.U_2)
+
+        logging.info("[%d/%d] | received %s's ciphertexts", received_num,
+                     SecretShareRequestHandler.U_1_num, self.client_address[0])
 
 
 class Server:
     def __init__(self):
         self.id = "0"
         self.host = socket.gethostname()
-        self.port = 10000
+        self.broadcast_port = 10000
+        self.signature_port = 20000
+        self.ss_port = 20001
 
-        self.tcp_server = socketserver.ThreadingTCPServer(
-            (self.host, self.port), SignatureRequestHandler)
+        self.signature_server = socketserver.ThreadingTCPServer(
+            (self.host, self.signature_port), SignatureRequestHandler)
+        self.ss_server = socketserver.ThreadingTCPServer(
+            (self.host, self.ss_port), SecretShareRequestHandler)
 
     def broadcast_signatures(self, port: int) -> list:
         """Broadcasts all users' key pairs and corresponding signatures.
@@ -54,7 +80,7 @@ class Server:
         # enable broadcasting mode
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        data = pickle.dumps(SignatureRequestHandler.signature_list)
+        data = pickle.dumps(SignatureRequestHandler.ka_pub_keys_map)
 
         SocketUtil.broadcast_msg(server, data, port)
 
--- entities/user.py
+++ entities/user.py
@@ -1,8 +1,10 @@
 import logging
 import pickle
+import random
 import socket
 
-from utils import KA, SIG, SocketUtil
+from utils import *
+from threading import Thread
 
 
 class User:
@@ -15,21 +17,36 @@ class User:
         self.pub_key_map = []
 
         self.c_pk = None
-        self.c_sk = None
+        self.__c_sk = None
         self.s_pk = None
-        self.s_sk = None
+        self.__s_sk = None
 
-        self.socket = socket.socket()
+        self.ka_pub_keys_map = None
+
+        self.__random_seed = None
 
     def gen_DH_pairs(self):
-        self.c_pk, self.c_sk = KA.gen()
-        self.s_pk, self.s_sk = KA.gen()
+        self.c_pk, self.__c_sk = KA.gen()
+        self.s_pk, self.__s_sk = KA.gen()
 
     def gen_signature(self):
         msg = pickle.dumps([self.c_pk, self.s_pk])
         signature = SIG.sign(msg, self.__priv_key)
 
         return signature
+
+    def ver_signature(self) -> bool:
+        status = True
+        for key, value in self.ka_pub_keys_map.items():
+            msg = pickle.dumps([value["c_pk"], value["s_pk"]])
+
+            res = SIG.verify(msg, value["signature"], self.pub_key_map[key])
+
+            if res is False:
+                status = False
+                logging.error("user {}'s signature is wrong!".format(key))
+
+        return status
 
     def send(self, data: bytes, host: str, port: int):
         """Sends data to host:port.
@@ -40,11 +57,12 @@ class User:
             port (int): the target port.
         """
 
-        self.socket.connect((host, port))
+        sock = socket.socket()
+        sock.connect((host, port))
 
-        SocketUtil.send_msg(self.socket, data)
+        SocketUtil.send_msg(sock, data)
 
-        self.socket.close()
+        sock.close()
 
     def listen_broadcast(self, port: int):
         """Listens to the server's broadcast, and saves all users' key pairs and corresponding signatures.
@@ -53,20 +71,49 @@ class User:
             port (int): the port used to broadcast the message.
         """
 
-        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # reuse port so we will be able to run multiple clients on single (host, port).
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
         # enable broadcasting mode
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-        client.bind(("", port))
+        sock.bind(("", port))
 
-        data = SocketUtil.recv_broadcast(client)
+        data = SocketUtil.recv_broadcast(sock)
 
-        self.signature_list = pickle.loads(data)
+        self.ka_pub_keys_map = pickle.loads(data)
 
         logging.info("received all signatures from the server")
 
-        client.close()
+        sock.close()
+
+    def gen_shares(self, U_1: list, t: int, host: str, ss_port: int):
+        # generates a random integer from - 2^20 to 2^20 (to be used as a seed for PRG)
+        self.__random_seed = random.randint(-2**20, 2**20)
+
+        n = len(U_1)
+
+        s_sk_shares = SS.share(self.__s_sk, t, n)
+        random_seed_shares = SS.share(self.__random_seed, t, n)
+
+        all_ciphertexts = []       # [{id: ciphertext}]
+
+        for i, v in enumerate(U_1):
+            if v == self.id:
+                continue
+
+            info = pickle.dumps([self.id, v, s_sk_shares[i], random_seed_shares[i]])
+
+            v_c_pk = self.ka_pub_keys_map[v]["c_pk"]
+            shared_key = KA.agree(self.__c_sk, v_c_pk)
+
+            ciphertext = AE.encrypt(shared_key, shared_key, info)
+
+            all_ciphertexts.append({v: ciphertext})
+
+        msg = pickle.dumps([self.id, all_ciphertexts])
+
+        # send all shares of s_sk and the random seed to the server
+        self.send(msg, host, ss_port)
